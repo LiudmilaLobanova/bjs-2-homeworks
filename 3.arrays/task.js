fdb08326ee31fe9ec660f10c131efa78b95f1e8b
LiudmilaLobanova <liudmilanl@gmail.com>
2023-05-20 19:35:16 -0700

    Поправила форматирование

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,14 +1,17 @@
 function compareArrays(arr1, arr2) {
-    return arr1.length === arr2.length &&
-    arr1.every((value, index) => value === arr2[index]);
+	return arr1.length === arr2.length &&
+		arr1.every((value, index) => value === arr2[index]);
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-    let count = (users.filter(item => item.gender == gender)).length;
+	let count = (users.filter(item => item.gender == gender)).length;
 	if (count == 0) {
 		return 0;
 	}
-	let result = (users.filter(item => item.gender == gender).map(users => users.age).reduce((sum, current) => sum + current)) / count;
+	let result = (users.filter(item => item.gender == gender)
+		.map(users => users.age)
+		.reduce((sum, current) => sum + current))
+		/ count;
 	return result;
-  
+
 }
